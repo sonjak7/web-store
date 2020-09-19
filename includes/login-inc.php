b171bc108ba97291cf32404397afb1c487ce35ff
satcom237 <satcom237@gmail.com>
2020-09-19 14:11:12 -0700

    Fixed login error

--- includes/login-inc.php
+++ includes/login-inc.php
@@ -10,36 +10,27 @@
 		$password = hash('sha256', $password);
 
     //Check if inputs are empty
-    if (($email_ID == "") || ($password == "")){
+    if (($email_ID == NULL) || ($password == NULL)){
       $message_status = "Empty inputs";;
       echo "<script type='text/javascript'>alert('$message_status');</script>";
       echo "<script type='text/javascript'> document.location = '../index.php?login==emptyinput'; </script>";
       exit();
     }
     else{
-      $query = "SELECT * FROM User WHERE email_ID=?";
-      $stmt = mysqli_stmt_init($conn); //Creating param query
-      mysqli_stmt_prepare($stmt, $query);
-      mysqli_stmt_bind_param($stmt, "s", $email_ID);
-      if(!(mysqli_stmt_execute($stmt))){
-        $message_status = "Wrong email_ID";
-        echo "<script type='text/javascript'>alert('$message_status');</script>";
-        echo "<script type='text/javascript'> document.location = '../index.php'; </script>";
-        exit();
-      }
-
       $query = "SELECT email_ID FROM User WHERE email_ID=? AND password=?";
       $stmt = mysqli_stmt_init($conn); //Creating param query
       mysqli_stmt_prepare($stmt, $query);
       mysqli_stmt_bind_param($stmt, "ss", $email_ID, $password);
-      if(!(mysqli_stmt_execute($stmt))){
-        $message_status = "Wrong password";
+      mysqli_stmt_execute($stmt);
+      $result = mysqli_stmt_get_result($stmt);
+      $resultCheck = mysqli_num_rows($result);
+      if($resultCheck != 1){
+        $message_status = "Wrong username or password";
         echo "<script type='text/javascript'>alert('$message_status');</script>";
         echo "<script type='text/javascript'> document.location = '../index.php'; </script>";
         exit();
       }
       else{
-        $result = mysqli_stmt_get_result($stmt);
         if($finalrow = mysqli_fetch_assoc($result)){ //Used to switch to page with logged-in user
             $_SESSION['email_ID'] = $finalrow['email_ID'];
             header("Location: ../index.php?login==success");
