29ee04c833c0c29b238a61c6d96c8893c507cb86
satcom237 <satcom237@gmail.com>
2020-09-19 13:23:06 -0700

    Fixed log-in error

--- includes/login-inc.php
+++ includes/login-inc.php
@@ -21,10 +21,7 @@
       $stmt = mysqli_stmt_init($conn); //Creating param query
       mysqli_stmt_prepare($stmt, $query);
       mysqli_stmt_bind_param($stmt, "s", $email_ID);
-      mysqli_stmt_execute($stmt);
-      $result = mysqli_stmt_get_result($stmt);
-      $resultCheck = mysqli_num_rows($result);
-      if($resultCheck < 1){
+      if(!(mysqli_stmt_execute($stmt))){
         $message_status = "Wrong email_ID";
         echo "<script type='text/javascript'>alert('$message_status');</script>";
         echo "<script type='text/javascript'> document.location = '../index.php'; </script>";
@@ -35,16 +32,14 @@
       $stmt = mysqli_stmt_init($conn); //Creating param query
       mysqli_stmt_prepare($stmt, $query);
       mysqli_stmt_bind_param($stmt, "ss", $email_ID, $password);
-      mysqli_stmt_execute($stmt);
-      $result = mysqli_stmt_get_result($stmt);
-      $resultCheck = mysqli_num_rows($result);
-      if($resultCheck < 1){
+      if(!(mysqli_stmt_execute($stmt))){
         $message_status = "Wrong password";
         echo "<script type='text/javascript'>alert('$message_status');</script>";
         echo "<script type='text/javascript'> document.location = '../index.php'; </script>";
         exit();
       }
       else{
+        $result = mysqli_stmt_get_result($stmt);
         if($finalrow = mysqli_fetch_assoc($result)){ //Used to switch to page with logged-in user
             $_SESSION['email_ID'] = $finalrow['email_ID'];
             header("Location: ../index.php?login==success");
