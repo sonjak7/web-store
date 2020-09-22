fc3a833ca633d2a5fa510b719acd35aea4884768
sonjak7 <sonjak737@gmail.com>
2020-09-22 14:25:44 -0700

    updated database for reviews page

--- projectx.sql
+++ projectx.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 14, 2020 at 01:14 AM
+-- Generation Time: Sep 22, 2020 at 11:23 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -44,9 +44,29 @@ CREATE TABLE `cart` (
 --
 
 INSERT INTO `cart` (`email_ID`, `product1`, `prod1_price`, `product2`, `prod2_price`, `product3`, `prod3_price`, `product4`, `prod4_price`) VALUES
-('satram@gmail.com', 16, 499.99, 3, 79.99, 2, 550, 0, 299),
-('sonjak@gmail.com', 1, 499.99, 2, 79.99, 5, 550, 4, 299),
-('the@the.com', 1, 499.99, 0, 79.99, 0, 550, 0, 299);
+('sonjak@gmail.com', 2, 499.99, 0, 79.99, 0, 550, 0, 0),
+('test@test.com', 0, 499.99, 0, 79.99, 0, 550, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` int(100) NOT NULL,
+  `firstname` varchar(32) NOT NULL,
+  `email_ID` varchar(32) NOT NULL,
+  `feedback` varchar(5000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `firstname`, `email_ID`, `feedback`) VALUES
+(33, 'sanjay', 'sonjak@gmail.com', 'good'),
+(35, 'test', 'test@test.com', 'bad');
 
 -- --------------------------------------------------------
 
@@ -58,7 +78,7 @@ CREATE TABLE `user` (
   `firstname` varchar(32) NOT NULL,
   `lastname` varchar(32) NOT NULL,
   `email_ID` varchar(32) NOT NULL,
-  `password` varchar(32) NOT NULL
+  `password` varchar(360) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -66,10 +86,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`firstname`, `lastname`, `email_ID`, `password`) VALUES
-('Sathya', 'Ram', 'satram@gmail.com', '12345678'),
-('sanjay', 'ram', 'sonjak@gmail.com', 'test_pass'),
-('test', 'test', 'test', '12345678'),
-('hi', 'bye', 'the@the.com', '123456789');
+('sanjay', 'ram', 'sonjak@gmail.com', '67325a653234fc9f72d5138fd040b322f7d2dc38afe16ad7b9205625920027e8'),
+('test', 'person', 'test@test.com', '82470495a07350a07ff3e2d40bc4de6957c13b7132d852b6f4610c7842479cf6');
 
 --
 -- Indexes for dumped tables
@@ -82,10 +100,26 @@ ALTER TABLE `cart`
   ADD PRIMARY KEY (`email_ID`);
 
 --
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`email_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
